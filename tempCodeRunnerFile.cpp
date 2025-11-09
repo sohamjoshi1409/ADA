87bce60e49653ae68705bce675bbbe4c7570f1c7
Soham Joshi <sohamjoshi1409@gmail.com>
2025-11-09 18:32:48 +0530

    Fourth

--- tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-for (auto &[v, w] : adj[u])
-        {
-            if (!inMST[v] && w < key[v])
-            {
-                key[v] = w;
-                pq.push({key[v], v});
-                parent[v] = u;
-            }
-        }
